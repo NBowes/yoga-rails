9d54a29267a35d9d3777b93ef176efa7ea7bacb8
NBowes <nathen.bowes@shopify.com>
2017-08-17 12:49:13 -0600

    added users + sessions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   get 'pages/about'
   get 'pages/poses'
   get 'pages/forum'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  get '/logout' => 'sessions#destroy'
+  post '/login' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
