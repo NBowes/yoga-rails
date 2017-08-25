1d3e16d1705e6c64407325109c3f9d692b06da57
NBowes <nathen.bowes@shopify.com>
2017-08-25 14:57:13 -0400

    changed local db for development

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170823170206) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "poses", force: :cascade do |t|
     t.string   "name"
     t.string   "difficulty"
