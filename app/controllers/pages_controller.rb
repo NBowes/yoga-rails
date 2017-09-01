faca064f4f06da3f650690d8450ca4dff89971f1
NBowes <nathen.bowes@shopify.com>
2017-09-01 16:26:29 -0400

    added homepage pose logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
+require 'time'
 class PagesController < ApplicationController
 
   def home
+    todays_poses = Pose.where('created_at <?',  DateTime.now.beginning_of_day)
+    pose_ids = todays_poses.pluck(:id)
+    day_today = Time.now.day
+    todays_pose_id = pose_ids[day_today % pose_ids.count]
+    @pose_today = Pose.find(todays_pose_id)
   end
 
   def about
